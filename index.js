d5fc88394aaf293520bcb5aefbb70bed99584662
Rajrupa Mitra <rajrupa.mitra@accenture.com>
2024-08-12 12:33:23 +0530

    DELETE and PUT api implementattion

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 //Middleware
 app.use(bodyParser.json());
+app.use(express.json());
 
 //SQL Server configuration
 var dbConfig = {
@@ -29,7 +30,7 @@ sql.connect(dbConfig, (err) => {
   }
 });
 
-//Define route for fetching data from SQL Server
+//FETCH-DATA(GET)
 app.get("/", (request, response) => {
   // Execute a SELECT query
   new sql.Request().query("SELECT * FROM bookstore", (err, result) => {
@@ -42,7 +43,8 @@ app.get("/", (request, response) => {
   });
 });
 
-app.post("/book", async (req, res) => {
+//INSERT-DATA (POST)
+app.post("/add-book", async (req, res) => {
   // Insert data into your database
   const result =
     await sql.query`INSERT INTO Bookstore (BookID, BookName, Author, price) VALUES (${req.body.BookID}, ${req.body.BookName}, ${req.body.Author}, ${req.body.price})`;
@@ -51,7 +53,41 @@ app.post("/book", async (req, res) => {
   res.status(200).json({ message: "Data inserted successfully", result });
 });
 
+//EDIT/UPDATE-DATA (PUT)
 
+app.put("/edit-Book/:BookID", async (req, res) => {
+  try {
+    // Update data in your database
+    const result = await sql.query`
+      UPDATE Bookstore
+      SET BookName = ${req.body.BookName},
+          Author = ${req.body.Author},
+          price = ${req.body.price}
+      WHERE BookID = ${req.params.BookID}`;
+
+    // Check if the update was successful
+    if (result.rowsAffected[0] > 0) {
+      res.status(200).json({ message: "Data updated successfully", result });
+    } else {
+      res.status(404).json({ message: "Book not found" });
+    }
+  } catch (error) {
+    // Handle any errors
+    res.status(500).json({ message: "An error occurred", error });
+  }
+});
+
+//DELETE-DATA(DELETE)
+
+app.delete("/delete-book/:BookID", async(req,res)=>{
+  try{
+    const result = await sql.query`DELETE FROM Bookstore WHERE BookID = ${req.params.BookID}`;
+    res.status(200).json({ message: "Data deleted successfully", result });
+  }
+  catch(error){
+    res.status(500).json({ message: "An error occurred", error });
+  }
+});
 
 // Start the server on port 3000
 app.listen(3000, () => {
